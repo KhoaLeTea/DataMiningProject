61c20a4d4fb635a93c4698392251cbbaf5e34a27
Madeleine Crouch <macr3115@colorado.edu>
2017-04-24 09:50:48 -0600

    Code to find businesses with more than 100 reviews and write to diff file. Currently broken.

---
+++ code/extract_businesses_many_reviews.py
@@ -0,0 +1,71 @@
+"""
+# start w/ >100 reviews (7845), drop to >30 w/ good processing flow (27387)
+"""
+import json
+# import plotly
+# import plotly.graph_objs as go
+
+infile = 'JSON/yelp_academic_dataset_business.json'
+outfile = 'JSON/businesses_many_reviews.json'
+offset, count = 0, 0
+# review_count = []
+over_hundred = 0
+write_str = ""
+
+
+def delete_contents(file_name):
+    with open(file_name, "w") as f:
+        pass
+
+delete_contents(outfile)
+with open(infile) as i_f, open(outfile, "a") as o_f:
+
+    if offset:
+        i_f.seek(offset, 0)  # seek from the beginning of the file to offset
+
+    for line in i_f:
+        obj = json.loads(line)
+        # review_count.append(obj['review_count'])
+        reviews = obj['review_count']
+        if reviews > 100:
+            business_id = obj['business_id']
+            name = obj['name']
+            city = obj['city']
+            state = obj['state']
+            latitude = obj['latitude']
+            longitude = obj['longitude']
+            stars = obj['stars']
+            json.dump({'business_id': business_id, 'name': name,
+                       'city': city, 'state': state, 'latitude': latitude,
+                       'longitude': longitude, 'stars': stars,
+                       'review_count': reviews}, o_f)
+            # o_f.write(write_str)
+            over_hundred += 1
+        if over_hundred > 2:
+            break
+        count += 1
+
+print over_hundred
+
+
+with open(outfile) as f:
+    for line in f:
+        data = json.loads(line)
+        print data['business_id']
+# trace0 = go.Histogram(x=review_count)
+# data = [trace0]
+# layout = go.Layout(
+#     title='Review Count by Business',
+#     xaxis=dict(
+#         title='Review Count',
+#         titlefont=dict(size=18)
+#     ),
+#     yaxis=dict(
+#         title='Number of Businesses',
+#         titlefont=dict(size=18)
+#     )
+# )
+#
+# fig = go.Figure(data=data, layout=layout)
+# plotly.offline.plot(fig, filename='review_count_histogram.html')
+print "Done!"
