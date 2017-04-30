3553c2835d22ec692e6f836e2246c1e15da8e89e
Madeleine Crouch <macr3115@colorado.edu>
2017-04-30 11:32:21 -0600

    Words and categories matching

---
+++ words_and_categories.py
@@ -0,0 +1,13 @@
+words = ["worst", "rude", "terrible", "horrible", "bad", "awful", "disgusting",
+         "bland", "tasteless", "gross", "mediocre", "overpriced", "worse",
+         "poor", "flavorless", "disappoint", "we", "us", "our", "ourselves",
+         "she", "he", "her", "him", "was", "were", "asked", "told", "said",
+         "did", "charged", "waited", "left", "took", "after", "then", "manager",
+         "waitress", "waiter", "customer", "attitude", "waste", "poisoning",
+         "money", "bill", "minutes"]
+
+# categories:
+service = []
+food = []
+price = []
+unresolved = []  # catchall for words that don't fit in other categories
